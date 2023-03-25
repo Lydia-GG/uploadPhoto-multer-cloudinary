1f86f1efd5aecb25a7d267c8e460a5176a13613a
Lydia Samir <67834807+Lydia-GG@users.noreply.github.com>
2023-03-25 18:02:05 +0100

    Update connectDB.js

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -1,1 +1,10 @@
 
+import mongoose from 'mongoose';
+
+mongoose.set('strictQuery', true);
+const connectDB = () => {
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log('database connected...');
+};
+
+export default connectDB;
