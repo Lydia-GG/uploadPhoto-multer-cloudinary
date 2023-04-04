ed5e47b4b2ca232251e000ab5fe21dfcc1f2b6bc
Lydia Samir <L-Samir@hotmail.com>
2023-04-04 19:04:41 +0200

    remove user route

--- routes/user.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { register } from '../controllers/user.js';
-
-router.post('/register', register);
-// router.post('/login');
-
-export default router;
